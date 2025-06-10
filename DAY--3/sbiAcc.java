9c5987d2be8ec89c8246aa18fd48265eaa2876f2
Tharunika21 <2326ka55@kgcas.com>
2025-06-10 12:00:47 +0530

    Add files via upload

---
+++ DAY--3/sbiAcc.java
@@ -0,0 +1,46 @@
+package program;
+class sbiAcc{
+    private String Accholder;
+    private int Balance;
+    sbiAcc(String Accholder , int Balance) {
+        this.Accholder=Accholder;
+        this.Balance=Balance;
+        
+    }
+    public String getAccholder() {
+        return Accholder;
+    }
+    public int getBalamce() {
+        return Balance;
+    }
+public void Deposit(int dep) {
+    if(Balance > 0) {
+        Balance += dep;
+        //Balance = balance + dep;
+        System.out.println("DepositeAmount:"+dep);
+        System.out.println("now current balance:+Balance");
+    }
+}
+public void setAccholder(String Accholder) {
+    this.Accholder=Accholder;
+}
+public void setWithdraw(int withdraw) {
+    Balance -= withdraw;
+    System.out.println("withdrawn amount:"+withdraw);
+    }
+}
+public class BankDetails {
+    public static void main(String[] args) {
+        sbiAcc ac=new sbiAcc("tharu",2000);
+        System.out.println("Account holder name:"+ac.getAccholder());
+        ac.setAccholder("harini");
+        System.out.println("update account holder:"+ac.getAccholder());
+        System.out.println("Balance:"+ac.getBalance());
+        ac.Deposit(3000);
+        ac.setWithdraw(20000);
+        System.out.println("Avail Balance:"+ac.getBalance());
+    }
+}
+
+    
+
