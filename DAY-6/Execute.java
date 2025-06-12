8900af90cf4acf65ce625944c5b5f05faaa5821c
Tharunika21 <2326ka55@kgcas.com>
2025-06-12 12:55:21 +0530

    Add files via upload

---
+++ DAY-6/Execute.java
@@ -0,0 +1,14 @@
+package Programs;
+public class Main extends Thread{
+    public void run() {
+        for(int i=1;i<=10;i++) {
+        System.out.println("HAI THREAD" +" "+i);
+    }
+}
+}
+class Execute{
+    public static void main(String[] args){
+        Main m=new Main();
+        m.start();
+    }
+}
